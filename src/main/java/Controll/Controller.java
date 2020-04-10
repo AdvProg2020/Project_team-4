dc0a35cb7667290eaf2f2eaff13825760b64e588
salehshoji <mohamadsaleh.shojai@yahoo.com>
2020-04-10 15:29:13 +0430

    add login and createAccount

--- src/main/java/Controll/Controller.java
+++ src/main/java/Controll/Controller.java
@@ -1,4 +1,31 @@
 package Controll;
 
+import Model.*;
+
 public class Controller {
+
+    public boolean validNewName(String username){
+        return Account.getAccountWithName(username) == null;
+    }
+
+    public void createAccount(String username, String type, String password){
+        //call create new Account method in Model
+    }
+
+    public int login(String username, String password){
+        Account account;
+        if((account = Account.getAccountWithName(username)) == null){
+            // means that "this name isn't exist"
+            return 2;
+        }
+        if(account.getPassWord().equals(password)){
+            // call login methos in model if needed
+            // means successfully logged in
+            return 1;
+        }
+        //means wrong password
+        return 3;
+    }
+
+
 }
