4bbc596bf21eb738284e168b05a68f60db104eaf
salehshoji <mohamadsaleh.shojai@yahoo.com>
2020-04-14 19:48:07 +0430

    add create account function in controller

--- src/main/java/Control/Controller.java
+++ src/main/java/Control/Controller.java
@@ -1,11 +1,43 @@
 package Control;
 
+import Model.Account;
+import Model.Customer;
+import Model.Manager;
+import Model.Seller;
+
 public class Controller {
-    private static Controller ourController = new Controller();
+    private final static Controller ourController = new Controller();
 
     public static Controller getOurController() {
         return ourController;
     }
+
+    public int requestCreateAccount(String type, String username, String password){
+        if (Account.getAccountWithName(username) != null){
+            return 2;
+        }
+        if(type.equals("customer")){
+            new Customer(username, password);
+            return 1;
+        }else if(type.equals("seller")){
+            Seller.addANewSeller(username, password);
+            return 1;
+        }else if(type.equals("manager")){
+            Manager.addANewManager(username, password);
+            return 1;
+        }else return 3;
+    }
+
+    public int requestLogin(String username, String password){
+        if(Account.getAccountWithName(username) == null){
+            return 2;
+        }if(Account.getAccountWithName(username).getPassWord().equals(password)){
+            //login this
+
+        }
+        return 3;
+    }
+
 
 }
 
