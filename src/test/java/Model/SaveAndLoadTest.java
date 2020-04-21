4dc7bfff7da867268fad629a5e82c52aa1a617cc
Amirhossein Arabzadeh <arabzadehamirhossein888@gmail.com>
2020-04-21 18:26:51 +0430

    .

---
+++ src/test/java/Model/SaveAndLoadTest.java
@@ -0,0 +1,19 @@
+package Model;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class SaveAndLoadTest {
+    private static Customer customer = new Customer("ali", "ali1230?aoksidjfkm']");
+    @Test
+    private static void saveTest() throws IOException {
+        customer.setCredit(50000);
+        customer.setEmail("email@email.com");
+        customer.setFirstName("ali");
+        customer.setLastName("aliiii");
+        customer.setPassWord("iuy7tr");
+        customer.setPhoneNumber("00000000000000000000");
+        SaveDataAndGetSystemOnBack.writeJSONAccount(customer);
+    }
+}
