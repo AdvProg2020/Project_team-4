aa20157941673cfbb16704d6f6b1c52b5b25c939
Amirhossein Arabzadeh <arabzadehamirhossein888@gmail.com>
2020-04-26 00:15:06 +0430

    First command

--- src/main/java/View/Menu/Menu.java
+++ src/main/java/View/Menu/Menu.java
@@ -1,6 +1,8 @@
 package View.Menu;
 
+import Control.Controller;
 import View.CommandsSource;
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -11,13 +13,66 @@ import static View.Outputs.*;
 
 public abstract class Menu {
 
+    private void createAccount() {
+        String input;
+        Matcher matcher;
+        String error = "please Enter type(customer|seller|manger) username \n" + "" +
+                "sample : \t create account customer ali\n" +
+                "for back write \"break\"";
+        Matcher matcher1 = CommandsSource.getField(error, "create\\saccount\\s(customer|seller|manger)\\s(\\S+)");
+        Matcher matcher2 = CommandsSource.getField("Enter a valid password", "(\\S+)");
+        if(printCreateAccountResult(Controller.getOurController().controllerNewAccount(matcher1.group(1), matcher1.group(2), matcher2.group(1)))){
+            return;
+        }
+    }
+
+    private void login() {
+        String input;
+        Matcher matcher;
+        while (!(input = scanner.nextLine()).trim().equalsIgnoreCase("break")) {
+            System.out.println("please Enter type username password \n" + "" +
+                    "sample : \tali 123\n" +
+                    "for back write \"break\"");
+            if((matcher = getMatcher(input, CommandsSource.LOGIN.getRegex())).matches()){
+                if(printLoginResult(Controller.getOurController().controllerLogin(matcher.group(1), matcher.group(2)))){
+                    return;
+                }
+            }
+        }
+    }
+
+
+    private void logout() {
+        printLogoutResult(Controller.getOurController().logout());
+    }
+
+
+    public void execute(int input) {
+        switch (input) {
+            case 1:
+                createAccount();
+                break;
+            case 2:
+                login();
+                break;
+            case 3:
+                logout();
+                break;
+        }
+    }
+
+    @Override
+    protected void execute() {
+
+    }
+
     protected static Scanner scanner = new Scanner(System.in);
     protected CommandsSource commands;
     protected ArrayList<String> options = new ArrayList<>();
 
-    public static Matcher getMatcher(String input, String regex) {
-        Pattern pattern = Pattern.compile(regex);
-        return pattern.matcher(input);
+
+    public static Scanner getScanner() {
+        return scanner;
     }
 
     protected void showCommands() {
@@ -27,5 +82,4 @@ public abstract class Menu {
         }
     }
 
-    protected abstract void execute();
 }
