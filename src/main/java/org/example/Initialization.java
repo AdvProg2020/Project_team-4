393ede6bed20978ffa86bfff4b91da496198780b
Amirhossein Arabzadeh <arabzadehamirhossein888@gmail.com>
2020-06-11 15:02:48 +0430

    create init page

---
+++ src/main/java/org/example/Initialization.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Initialization {
+    @FXML
+    private TextField initUserName;
+    @FXML
+    private TextField initPassWord;
+
+}
