6df101a36d3cc1d9ba7adde54854de7795c70aed
Amirhossein Arabzadeh <arabzadehamirhossein888@gmail.com>
2020-06-24 14:58:16 +0430

    add pay

--- src/main/java/org/example/Pay.java
+++ src/main/java/org/example/Pay.java
@@ -1,4 +1,56 @@
 package org.example;
 
-public class Pay {
+import Control.Controller;
+import Model.Off;
+import Model.Seller;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class Pay implements Initializable {
+
+    ArrayList<TextField> textFields = new ArrayList<>();
+
+    public TextField addressField;
+    public TextField phoneField;
+    public TextField offCodeField;
+    public Button payButton;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+    public void checkPhone(ActionEvent actionEvent) {
+        if (phoneField.getText()!=null && !phoneField.getText().trim().equalsIgnoreCase("")) {
+            offCodeField.setVisible(true);
+            payButton.setVisible(true);
+        }
+    }
+
+    public void checkAddr(ActionEvent actionEvent) {
+        if (addressField.getText()!=null && !addressField.getText().trim().equalsIgnoreCase("")) {
+            offCodeField.setVisible(true);
+            payButton.setVisible(true);
+        }
+    }
+
+    public void pay(ActionEvent actionEvent) {
+        if (offCodeField.getText() != null) {
+            Controller.getOurController().pay(offCodeField.getText().trim());
+        } else {
+            Controller.getOurController().pay("");
+        }
+    }
 }
