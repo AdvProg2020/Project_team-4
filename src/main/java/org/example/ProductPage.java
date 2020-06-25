919a99b26c67dead68b7d2eba66a7f1a75d5633f
MohammadHossein Dolatabadi <m.h.dolatabadi.a@gmail.com>
2020-06-25 23:19:41 +0430

    add seller menu button

--- src/main/java/org/example/ProductPage.java
+++ src/main/java/org/example/ProductPage.java
@@ -2,11 +2,12 @@ package org.example;
 
 
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
 
 public class ProductPage {
     public TextField comment;
     public TextField score;
     public Button addButton;
-
+    public MenuButton sellers;
 }
