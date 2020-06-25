b9b3939d29d2f0d057240753ca31f53cde0e5d8a
MohammadHossein Dolatabadi <m.h.dolatabadi.a@gmail.com>
2020-06-25 23:22:10 +0430

    implement add new comment

--- src/main/java/org/example/ProductPage.java
+++ src/main/java/org/example/ProductPage.java
@@ -3,6 +3,7 @@ package org.example;
 
 import Control.Controller;
 import Model.Product;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.TextField;
@@ -13,10 +14,16 @@ public class ProductPage {
     public Button addButton;
     public MenuButton sellers;
     public Product product;
+    public Alert alert;
     public ProductPage(Product product) {
         this.product = product;
     }
     public void addToCart() {
         Controller.getOurController().requestAddProductToCart(product.getProductBarcode());
+    }
+    public void addComment() {
+        Controller.getOurController().newComment(comment.getText());
+        alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setContentText("Successfully added.");
     }
 }
