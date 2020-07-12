585fc0ab92ecec4019d84af30b508983292717fc
MohammadHossein Dolatabadi <m.h.dolatabadi.a@gmail.com>
2020-07-12 11:30:49 +0430

    implement base of wallet

--- src/main/java/Model/Wallet.java
+++ src/main/java/Model/Wallet.java
@@ -1,4 +1,31 @@
 package Model;
 
 public class Wallet {
+    double credit;
+    double leastCredit;
+    Account account;
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public void getMoney(double amount){
+        if(credit - amount > leastCredit)
+            credit -= amount;
+    }
+    public void charge(double amount){
+        credit += amount;
+    }
 }
